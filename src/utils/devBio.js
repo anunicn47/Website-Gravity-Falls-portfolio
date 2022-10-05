3390f8c2b6b47940e0f57e81d08fcbfb0c436c65
Ananya <90745373+anunicn47@users.noreply.github.com>
2022-10-05 14:32:14 +0000

    Update devBio.js

--- src/utils/devBio.js
+++ src/utils/devBio.js
@@ -1,5 +1,5 @@
 const devBio = {
-    description: `Hello! I'm Nneka, a full-stack developer based in The Hague who enjoys building engaging things with code. I develop exceptional websites and web apps that provide awe-inducing, pixel-perfect user experiences with clean, efficient, and scalable code.
+    description: `Hello! I'm Karan Raj, a full-stack developer based in The Hague who enjoys building engaging things with code. I develop exceptional websites and web apps that provide awe-inducing, pixel-perfect user experiences with clean, efficient, and scalable code.
 
     Shortly after completing a 9-month full-stack course from Winc Academy, I joined the development team at Bravoure as a front-end intern where I work on a wide variety of interesting and meaningful projects on a daily basis.
     
@@ -14,7 +14,7 @@ const devBio = {
         'GSAP'],
     image: '/assets/images/about/devAvatar.png',
     webpImage: '/assets/images/about/devAvatar.webp',
-    caption: 'Depiction of Nika "C-137" cooking up a master plan'
+    caption: 'Depiction of Karan cooking up a master plan'
 }
 
 export default devBio;

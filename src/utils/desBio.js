1a85863d532a1dfe97c7b70e670089c4fa0019cb
Ananya <90745373+anunicn47@users.noreply.github.com>
2022-10-05 12:52:25 +0000

    Update desBio.js

--- src/utils/desBio.js
+++ src/utils/desBio.js
@@ -1,5 +1,5 @@
 const designBio = {
-    description: `My name is Nneka, (feel free to call me Nika, as many people struggle to pronounce the name). I'm an illustrator/ visual designer currently residing in The Hague.
+    description: `Hey I'm Karan, (feel free to call me Raj, as many people struggle to pronounce the name). I'm an illustrator/ visual designer currently residing in India.
 
     Coming from a creative background taught me to carefully consider my audience and what experience I intend for them to have through my work. The projects I'm most passionate to work on are those where I get the opportunity to tell a story through UI effects, animations, and creating a dynamic user experience.
     
@@ -13,7 +13,7 @@ const designBio = {
         'Adobe Premier'],
     image: '/assets/images/about/designAvatar.png',
     webpImage: '/assets/images/about/designAvatar.webp',
-    caption: 'Depiction of Nika when in the vicinity of sushi. (you have been warned)'
+    caption: 'Depiction of Karan Raj.'
 }
 
 export default designBio;
